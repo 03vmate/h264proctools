308490fdf40b045a4f5e899a03492b6199fe9267
Mate V <47510515+03vmate@users.noreply.github.com>
2024-10-20 19:15:45 +0200

    Initial commit

---
+++ build_frame_index.py
@@ -0,0 +1,28 @@
+import sys
+from readh264 import readh264
+import glob
+import json
+import os
+
+h264_files = sorted(glob.glob(os.path.join(sys.argv[1], '*.h264')))
+
+frame_map = {}
+total_count = 0
+for h264_file in h264_files:
+    frames, frame_count = readh264(h264_file, -1)
+    print(f"{h264_file}: {frame_count} frames")
+    frame_map[h264_file] = {"frame_count": frame_count, "starting_index": total_count}
+    total_count += frame_count
+
+# Create filename that does not exist
+base_name = "frame_map"
+index = 0
+if os.path.exists(f"{base_name}.json"):
+    base_name = f"{base_name}_{index}"
+
+while os.path.exists(f"{base_name}_{index}.json"):
+    index += 1
+
+# Save the frame map to a JSON file
+with open(f"{base_name}_{index}.json", "w") as f:
+    json.dump(frame_map, f)
---
+++ readh264.py
@@ -0,0 +1,68 @@
+import cv2
+from typing import Final, List, Optional
+from numpy import ndarray
+
+
+
+def readh264(h264_file: str, nth_frame: Optional[int] = None) -> (List[ndarray], int):
+    """
+    Read an h264 file and return a list of frames. Optionally, only read every nth frame.
+    :param h264_file: The path to the h264 file.
+    :param nth_frame: Only read every nth frame. If None, read all frames. If -1, read no frames.
+    :return: A list of frames read and the total number of frames in the video(counting non-read frames).
+    """
+    cap = cv2.VideoCapture(h264_file)
+
+    if not cap.isOpened():
+        raise Exception(f"Could not open {h264_file}")
+
+    frames: List[ndarray] = []
+    frame_count: int = 0
+    consecutive_errors: int = 0
+    max_consecutive_errors = 10 # If we encounter n consecutive errors, we assume we have reached the end of the video. Since the h264 files are stream captures, a few errors are expected.
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            consecutive_errors += 1
+            if consecutive_errors >= max_consecutive_errors:
+                break
+        else:
+            consecutive_errors = 0
+            frame_count += 1
+            if nth_frame is not None and (frame_count % nth_frame != 0 or nth_frame == -1):
+                continue
+            frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    cap.release()
+    return frames, frame_count
+
+def readh264frame(h264_file: str, frame_index: int) -> ndarray:
+    """
+    Read a specific frame from an h264 file.
+    :param h264_file: The path to the h264 file.
+    :param frame_index: The index of the frame to read.
+    :return: The frame at the specified index.
+    """
+    cap = cv2.VideoCapture(h264_file)
+
+    if not cap.isOpened():
+        raise Exception(f"Could not open {h264_file}")
+
+    frame = None
+    frame_count: int = 0
+    consecutive_errors: int = 0
+    max_consecutive_errors = 10  # If we encounter n consecutive errors, we assume we have reached the end of the video. Since the h264 files are stream captures, a few errors are expected.
+
+    while frame_count < frame_index:
+        ret, frame = cap.read()
+        if not ret:
+            consecutive_errors += 1
+            if consecutive_errors >= max_consecutive_errors:
+                break
+        else:
+            consecutive_errors = 0
+            frame_count += 1
+
+    cap.release()
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) if frame is not None else None
